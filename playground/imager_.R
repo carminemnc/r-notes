3b75e1ae2accdc7f1686d81dabff20d20beb5de8
carminemnc <carmine.mnc@gmail.com>
2025-05-08 09:32:36 +0200

    rearranging

---
+++ playground/imager_.R
@@ -0,0 +1,18 @@
+library(hexSticker)
+
+# Versione 2 - Con più controlli
+sticker(
+  subplot = "nebula.png",
+  package = "causalwiz",
+  p_size = 20,
+  p_color = "#D3D3D3", # text color
+  s_x = 1,
+  s_y = 1,
+  s_width = 1,
+  s_height = 1,
+  h_size = 0.7,
+  h_fill = "#000000", # background
+  h_color = "#D3D3D3", # border
+  filename = "logo.png"
+)
+
